3f8d57a01798d8d6edbe9fc93c539905be2f3032
shi <shi@shi>
2023-01-22 19:22:21 +0900

    two_level_pipeline_adder

---
+++ pipelineadder.v
@@ -0,0 +1,36 @@
+module pipeline_8bit_adder (enable,rst,a,b,cin,cout,sum);
+
+input [7:0] a,b;
+output reg[7:0] sum;
+output reg cout;
+input cin,rst,enable;
+reg[3:0] temp_a;
+reg[3:0] temp_b;
+
+reg temp_cin;
+reg[3:0]low_bit_sum;
+reg[3:0]high_bit_sum;
+always @(posedge enable  or negedge rst) begin
+    if (~rst) begin
+     sum=0;
+     cout=0;   
+    end
+    else begin
+    {temp_cin,low_bit_sum}<=a[3:0]+b[3:0]+cin; 
+    temp_a <= a[7:4];
+    temp_b <= b[7:4];
+    end
+end
+always @( posedge  enable or negedge rst) begin
+    if (~rst) begin
+     sum=0;
+     cout=0;   
+    end
+    else begin
+    {cout,sum[7:4]}<=temp_a+temp_b+temp_cin;
+    {cout,high_bit_sum}<=temp_a+temp_b+temp_cin;
+    sum[3:0]<=low_bit_sum; 
+    end
+end
+
+endmodule
---
+++ tb_8_bitpipeline_adder.v
@@ -0,0 +1,39 @@
+module tb_8_bitadder ();
+
+reg [7:0] a,b;
+output [7:0] sum;
+output  cout;
+ reg cin,rst,enable;
+parameter delay=80; 
+pipeline_8bit_adder m1(enable,rst,a,b,cin,cout,sum);
+
+
+initial begin
+   $dumpfile("dump.vcd"); 
+   $dumpvars;
+end
+
+initial begin
+  enable=0;
+     forever #10 enable = ~enable;  
+end 
+initial begin                                                  
+#10 a=8'b0;b=8'b0;cin=1'b0; rst=1;                
+#20 a=1;b=1;
+#20 a=20;b=20;cin=1;
+#20 a=75;b=75;cin=1;
+#20 a=128;b=128;cin=0;
+#20 a=200;b=200;cin=0;                                                                                 
+end 
+// initial begin
+//   clk=0;
+//      forever #10 clk = ~clk;  
+// end 
+
+
+initial begin 
+    #200 $finish;
+end
+
+
+endmodule
