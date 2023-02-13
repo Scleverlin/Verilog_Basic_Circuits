3f29f3e889ea914aa0383dd5161d04c39ed2e469
ChenlinShi <scleverlin@Gmail.com>
2023-02-13 23:03:56 +0900

    for g14

--- multiplier/serial_multiplier.v
+++ multiplier/serial_multiplier.v
@@ -2,8 +2,9 @@ module serial_multiplier (
     a,b,out
 );
 input [7:0] a,b;
-output [15:0] out;
+output reg [15:0] out;
 // input clk , rst;
+
 
 wire [15:0] bit_0_mux ;
 wire [15:0] bit_1_mux ;
@@ -22,6 +23,8 @@ wire [15:0] bit_4_5_sum;
 wire [15:0] bit_5_6_sum;
 wire [15:0] bit_6_7_sum;
 
+wire cin,cout;
+assign cin=0;
 
 
 assign bit_0_mux=(a[0])?b:0;
@@ -33,15 +36,37 @@ assign bit_5_mux=(a[5])?{b,5'b0}:0;
 assign bit_6_mux=(a[6])?{b,6'b0}:0;
 assign bit_7_mux=(a[7])?{b,7'b0}:0;
 
-assign bit_0_1_sum=bit_0_mux+bit_1_mux;
-assign bit_1_2_sum=bit_0_1_sum+bit_2_mux;
-assign bit_2_3_sum=bit_1_2_sum+bit_3_mux;
-assign bit_3_4_sum=bit_2_3_sum+bit_4_mux;
-assign bit_4_5_sum=bit_3_4_sum+bit_5_mux;
-assign bit_6_7_sum=bit_4_5_sum+bit_6_mux;
+adder_16_bit_comb u0(bit_0_mux,bit_1_mux,cin,cout,bit_0_1_sum);
+adder_16_bit_comb u1(bit_0_1_sum,bit_2_mux,cin,cout,bit_1_2_sum);
+adder_16_bit_comb u2(bit_1_2_sum,bit_2_mux,cin,cout,bit_2_3_sum);
+adder_16_bit_comb u3(bit_2_3_sum,bit_2_mux,cin,cout,bit_3_4_sum);
+adder_16_bit_comb u4(bit_3_4_sum,bit_2_mux,cin,cout,bit_4_5_sum);
+adder_16_bit_comb u5(bit_4_5_sum,bit_2_mux,cin,cout,bit_6_7_sum);
 
- assign out= bit_6_7_sum+bit_7_mux;
 
+
+// assign bit_0_1_sum=bit_0_mux+bit_1_mux;
+// assign bit_1_2_sum=bit_0_1_sum+bit_2_mux;
+// assign bit_2_3_sum=bit_1_2_sum+bit_3_mux;
+// assign bit_3_4_sum=bit_2_3_sum+bit_4_mux;
+// assign bit_4_5_sum=bit_3_4_sum+bit_5_mux;
+// assign bit_6_7_sum=bit_4_5_sum+bit_6_mux;
+
+assign out= bit_6_7_sum+bit_7_mux;
 
 
 endmodule //serial_multiplier
+
+module adder_16_bit_comb(
+a,b,cin,cout,sum
+);
+
+input [15:0] a,b;
+input cin;
+output  reg cout;
+output [15:0] sum;
+
+assign {cout,sum}=a+b+cin;
+
+
+endmodule
