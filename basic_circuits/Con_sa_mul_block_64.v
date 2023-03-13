445f7ce4bd45698dea7d9e2079f95314aae35f21
ChenlinShi <scleverlin@gmail.com>
2023-03-13 18:24:05 +0900

    update

--- basic_circuits/Con_sa_mul_block_64.v
+++ basic_circuits/Con_sa_mul_block_64.v
@@ -98,7 +98,7 @@ output cout;
 wire [15:0]bit_carry,bit_carry_1;
 wire [15:0]sum_1,sum_2;
 // cin==1
-ADD_full adder1(bit_carry[0],sum_1[0],a[0],b[0],1'b1);
+ADD_full adder1(bit_carry[0],sum_1[0],a[0],b[0],cin);
 ADD_full adder2(bit_carry[1],sum_1[1],a[1],b[1],bit_carry[0]);
 ADD_full adder3(bit_carry[2],sum_1[2],a[2],b[2],bit_carry[1]);
 ADD_full adder4(bit_carry[3],sum_1[3],a[3],b[3],bit_carry[2]);
@@ -115,7 +115,7 @@ ADD_full adder14(bit_carry[13],sum_1[13],a[13],b[13],bit_carry[12]);
 ADD_full adder15(bit_carry[14],sum_1[14],a[14],b[14],bit_carry[13]);
 ADD_full adder16(bit_carry[15],sum_1[15],a[15],b[15],bit_carry[14]);
 //cin==0
-ADD_full adder_1_1(bit_carry_1[0],sum_2[0],a[0],b[0],1'b0);
+ADD_full adder_1_1(bit_carry_1[0],sum_2[0],a[0],b[0],`cin);
 ADD_full adder_1_2(bit_carry_1[1],sum_2[1],a[1],b[1],bit_carry_1[0]);
 ADD_full adder_1_3(bit_carry_1[2],sum_2[2],a[2],b[2],bit_carry_1[1]);
 ADD_full adder_1_4(bit_carry_1[3],sum_2[3],a[3],b[3],bit_carry_1[2]);
@@ -150,7 +150,8 @@ multiplexer mul_sum_13 (sum_1[13],sum_2[13],cin,sum[13]);
 multiplexer mul_sum_14 (sum_1[14],sum_2[14],cin,sum[14]);
 multiplexer mul_sum_15 (sum_1[15],sum_2[15],cin,sum[15]);
 
-multiplexer mul_1(bit_carry[15],bit_carry_1[15],cin,cout);
+// multiplexer mul_1(bit_carry[15],bit_carry_1[15],cin,cout);
+assign cout=bit_carry[15] | bit_carry_1[15];
 
 endmodule //CSelectAdder_4bit
 
@@ -163,7 +164,7 @@ output cout;
 wire [7:0]bit_carry,bit_carry_1;
 wire [7:0]sum_1,sum_2;
 // cin==1
-ADD_full adder1(bit_carry[0],sum_1[0],a[0],b[0],1'b1);
+ADD_full adder1(bit_carry[0],sum_1[0],a[0],b[0],cin);
 ADD_full adder2(bit_carry[1],sum_1[1],a[1],b[1],bit_carry[0]);
 ADD_full adder3(bit_carry[2],sum_1[2],a[2],b[2],bit_carry[1]);
 ADD_full adder4(bit_carry[3],sum_1[3],a[3],b[3],bit_carry[2]);
@@ -172,7 +173,7 @@ ADD_full adder6(bit_carry[5],sum_1[5],a[5],b[5],bit_carry[4]);
 ADD_full adder7(bit_carry[6],sum_1[6],a[6],b[6],bit_carry[5]);
 ADD_full adder8(bit_carry[7],sum_1[7],a[7],b[7],bit_carry[6]);
 //cin==0
-ADD_full adder_1_1(bit_carry_1[0],sum_2[0],a[0],b[0],1'b0);
+ADD_full adder_1_1(bit_carry_1[0],sum_2[0],a[0],b[0],`cin);
 ADD_full adder_1_2(bit_carry_1[1],sum_2[1],a[1],b[1],bit_carry_1[0]);
 ADD_full adder_1_3(bit_carry_1[2],sum_2[2],a[2],b[2],bit_carry_1[1]);
 ADD_full adder_1_4(bit_carry_1[3],sum_2[3],a[3],b[3],bit_carry_1[2]);
@@ -191,7 +192,8 @@ multiplexer mul_sum_5 (sum_1[5],sum_2[5],cin,sum[5]);
 multiplexer mul_sum_6 (sum_1[6],sum_2[6],cin,sum[6]);
 multiplexer mul_sum_7 (sum_1[7],sum_2[7],cin,sum[7]);
 
-multiplexer mul_1(bit_carry[7],bit_carry_1[7],cin,cout);
+// multiplexer mul_1(bit_carry[7],bit_carry_1[7],cin,cout);
+assign cout=bit_carry[7] | bit_carry_1[7];
 endmodule //CSelectAdder_4bit
 
 module ADD_full(output c_out,sum, input a,b,cin);
