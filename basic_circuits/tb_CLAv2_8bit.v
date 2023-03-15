41db2cd78e82677a077cfba5a59aedea1aef4602
shi <shi@shi>
2023-03-15 18:09:43 +0900

    update

--- basic_circuits/tb_CLAv2_8bit.v
+++ basic_circuits/tb_CLAv2_8bit.v
@@ -2,13 +2,13 @@ module tb_CLAv2_8bit (
     
 );
 
-reg [7:0] a,b;
-wire [7:0] sum;
+reg [15:0] a,b;
+wire [15:0] sum;
 wire  cout;
 
 reg cin;
 parameter delay=20; 
-CLAv2_8bit cla (a,b,cin,sum,cout);
+CLA_16_bit_block cla (a,b,cin,sum,cout);
 
 
 initial begin
@@ -23,7 +23,14 @@ initial begin
 #delay a=20;b=20;cin=1;
 #delay a=75;b=75;cin=1;
 #delay a=128;b=128;cin=0;
-#delay a=200;b=20;cin=0;                                                                                 
+#delay a=200;b=20;cin=0;      
+#delay a=1024;b=1024;cin=0;
+#delay a=2048;b=2048;cin=0;
+#delay a=4096;b=4096;cin=0;
+#delay a=8192;b=8192;cin=0;
+#delay a=16384;b=16384;cin=0;         
+#delay a=2000;b=2230;cin=0;                                                                                 
+
 end 
 
 // initial begin
