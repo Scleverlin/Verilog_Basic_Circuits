e908e3b3cef1b99bc385d66dad891e4c010f48e7
ChenlinShi <scleverlin@Gmail.com>
2023-03-16 22:01:03 +0900

    update

--- basic_circuits/Con_sa_mul_block_64.v
+++ basic_circuits/Con_sa_mul_block_64.v
@@ -71,6 +71,27 @@ CSelectAdder_16bit csa_16_4 (.a(a[63:48]), .b(b[63:48]), .cin(bit_carry[2]), .su
 
 endmodule //Con_sa_8_bit_block_64
 
+module Con_sa_4_bit_block_64 (    
+    a,b,cin,sum,cout
+);
+input [63:0]a,b;
+input cin;
+output [63:0]sum;
+output cout;
+wire [14:0]bit_carry;
+
+CSelectAdder_4bit  csa_4_1 (.a(a[3:0]), .b(b[3:0]), .cin(cin), .sum(sum[3:0]), .cout(bit_carry[0]));
+CSelectAdder_4bit  csa_4_2 (.a(a[7:4]), .b(b[7:4]), bit_carry[0], .sum(sum[7:4]), .cout(bit_carry[1]));
+CSelectAdder_4bit  csa_4_2 (.a(a[11:8]), b[11:8], bit_carry[1], .sum(sum[11:7]), .cout(bit_carry[2]));
+
+
+
+
+
+
+
+endmodule //Con_sa_8_bit_block_64
+
 module Con_sa_8_bit_block_64 (    
     a,b,cin,sum,cout
 );
@@ -213,4 +234,48 @@ input [15:0]a,b;
 input sel;
 output [15:0]out;
 assign out= (sel)?a:b;
+endmodule 
+
+module CSelectAdder_4bit (
+    a,b,cin,sum,cout
+);
+
+input [3:0]a,b;
+input cin;
+output [3:0]sum;
+output cout;
+
+wire [3:0]bit_carry,bit_carry_1;
+wire [3:0]sum_1,sum_2;
+
+// cin==1
+ADD_full adder1(bit_carry[0],sum_1[0],a[0],b[0],1'b1);
+ADD_full adder2(bit_carry[1],sum_1[1],a[1],b[1],bit_carry[0]);
+ADD_full adder3(bit_carry[2],sum_1[2],a[2],b[2],bit_carry[1]);
+ADD_full adder4(bit_carry[3],sum_1[3],a[3],b[3],bit_carry[2]);
+//cin==0
+ADD_full adder_1_1(bit_carry_1[0],sum_2[0],a[0],b[0],1'b0);
+ADD_full adder_1_2(bit_carry_1[1],sum_2[1],a[1],b[1],bit_carry_1[0]);
+ADD_full adder_1_3(bit_carry_1[2],sum_2[2],a[2],b[2],bit_carry_1[1]);
+ADD_full adder_1_4(bit_carry_1[3],sum_2[3],a[3],b[3],bit_carry_1[2]);
+
+multiplexer_4_bit mul_8(sum_1,sum_2,cin,sum);
+
+
+assign cout = (~cin&bit_carry[2] )|(cin &bit_carry_1[2]);
+
+
+endmodule //CSelectAdder_4bit
+
+
+module  multiplexer_4_bit (a,b,sel,out
+    
+);
+
+input [3:0]a,b;
+input sel;
+output [3:0]out;
+assign out= (sel)?a:b;
+
+
 endmodule 
