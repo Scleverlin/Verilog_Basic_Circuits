d9acee0bdc55da1783f21cace6648ad339f11328
ChenlinShi <scleverlin@Gmail.com>
2023-10-27 14:43:42 +0900

    update

--- SRT_Divider/csrc/filelist.cu
+++ SRT_Divider/csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _54223_archive_1.so
-_54223_archive_1.so : archive.29/_54223_archive_1.a
+ARCHIVE_OBJS += _121492_archive_1.so
+_121492_archive_1.so : archive.30/_121492_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_54223_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_121492_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_54223_archive_1.so $@
+	@ln -sf .//../simv.daidir//_121492_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.29/_prev_archive_1.a
+_prev_archive_1.so : archive.30/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
