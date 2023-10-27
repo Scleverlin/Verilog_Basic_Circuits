3d774ee283b9cdbf4343f87cf51d3db8b2b3bb63
ChenlinShi <scleverlin@Gmail.com>
2023-10-27 15:26:55 +0900

    update

--- SRT_Divider/csrc/filelist.cu
+++ SRT_Divider/csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _121492_archive_1.so
-_121492_archive_1.so : archive.30/_121492_archive_1.a
+ARCHIVE_OBJS += _504_archive_1.so
+_504_archive_1.so : archive.31/_504_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_121492_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_504_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_121492_archive_1.so $@
+	@ln -sf .//../simv.daidir//_504_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.30/_prev_archive_1.a
+_prev_archive_1.so : archive.31/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
@@ -18,9 +18,6 @@ _prev_archive_1.so : archive.30/_prev_archive_1.a
 
 
 
-VCS_CU_ARC0 =_cuarc0.so
-
-VCS_CU_ARC_OBJS0 =objs/amcQw_d.o 
 
 
 O0_OBJS =
@@ -31,12 +28,6 @@ $(O0_OBJS) : %.o: %.c
 
 %.o: %.c
 	$(CC_CG) $(CFLAGS_CG) -c -o $@ $<
-
-$(VCS_CU_ARC0) : $(VCS_CU_ARC_OBJS0)
-	$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//$(VCS_CU_ARC0) $(VCS_CU_ARC_OBJS0)
-	rm -f $(VCS_CU_ARC0)
-	@ln -sf .//../simv.daidir//$(VCS_CU_ARC0) $(VCS_CU_ARC0)
-
 CU_UDP_OBJS = \
 
 
@@ -44,7 +35,7 @@ CU_LVL_OBJS = \
 SIM_l.o 
 
 MAIN_OBJS = \
+objs/amcQw_d.o 
 
-
-CU_OBJS = $(MAIN_OBJS) $(ARCHIVE_OBJS) $(VCS_CU_ARC0) $(CU_UDP_OBJS) $(CU_LVL_OBJS)
+CU_OBJS = $(MAIN_OBJS) $(ARCHIVE_OBJS) $(CU_UDP_OBJS) $(CU_LVL_OBJS)
 
---
+++ SRT_Divider/tb_post_processing.sv
@@ -0,0 +1,39 @@
+`include "post_processing.sv"
+module tb_pp_q();
+ logic [23:0] result;
+ logic [4:0]shift_nums;
+ logic right_shift;
+ logic resultsign;
+ logic [7:0] current_exponent;
+logic [31:0]quotient;
+
+
+post_processing pp(  //convert the final quotient to be IEEE-754
+result,
+shift_nums,
+ right_shift,
+resultsign,
+ current_exponent,
+quotient
+);
+
+
+initial begin
+       $dumpfile("dump.vcd");
+       $dumpvars(0, pp); 
+end
+
+    // 测试向量生成
+  initial begin
+
+        #10 shift_nums = 5'b00001;
+            current_exponent = 8'd127;
+            right_shift = 1'b0;
+            resultsign = 1'b0;
+            result = {1'b0,1'b1,22'b0};
+
+
+        #200 $finish; // 结束仿真
+    end
+
+endmodule
