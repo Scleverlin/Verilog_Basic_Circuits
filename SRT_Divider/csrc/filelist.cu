4d03c7ca024aceed6b083c50ea26dc9440ae621e
ChenlinShi <scleverlin@Gmail.com>
2023-10-27 15:49:15 +0900

    update

--- SRT_Divider/csrc/filelist.cu
+++ SRT_Divider/csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _3187_archive_1.so
-_3187_archive_1.so : archive.32/_3187_archive_1.a
+ARCHIVE_OBJS += _9655_archive_1.so
+_9655_archive_1.so : archive.40/_9655_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_3187_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_9655_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_3187_archive_1.so $@
+	@ln -sf .//../simv.daidir//_9655_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.32/_prev_archive_1.a
+_prev_archive_1.so : archive.40/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
--- SRT_Divider/tb_post_processing.sv
+++ SRT_Divider/tb_post_processing.sv
@@ -1,36 +1,55 @@
 `include "post_processing.sv"
-module tb_pp_q();
- logic [23:0] result;
- logic [4:0]shift_nums;
- logic right_shift;
- logic resultsign;
- logic [7:0] current_exponent;
-logic [31:0]quotient;
 
+module tb_post_processing();
 
-post_processing pp(  //convert the final quotient to be IEEE-754
-result,
-shift_nums,
- right_shift,
-resultsign,
- current_exponent,
-quotient
-);
+    // 声明信号
+    logic clk;
+    logic rst;
+    logic [23:0] result;
+    logic [4:0] shift_nums;
+    logic right_shift;
+    logic resultsign;
+    logic [7:0] current_exponent;
+    logic [31:0] quotient;
 
-
-initial begin
-       $dumpfile("dump.vcd");
-       $dumpvars(0, pp); 
-end
+    // 实例化DUT (Device Under Test)
+    post_processing dut (
+        .result(result),
+        .shift_nums(shift_nums),
+        .right_shift(right_shift),
+        .resultsign(resultsign),
+        .current_exponent(current_exponent),
+        .quotient(quotient)
+    );
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, dut);
+    end
+    // 时钟生成器
+    always begin
+        #5 clk = ~clk;
+    end
+    // 波形文件生成
 
     // 测试向量生成
-  initial begin
-shift_nums = 5'b00001;
-    current_exponent = 8'd127;
-    right_shift = 1'b0;
-    resultsign = 1'b0;
-    result = {1'b0,1'b1,22'b0};
-#200 $finish; // 结束仿真
+    initial begin
+        clk = 0;
+        rst = 1;
+        #10;
+        rst = 0;
+        
+        // 赋予测试向量的值
+        shift_nums = 5'b00001;
+        current_exponent = 8'd127;
+        right_shift = 1'b0;
+        resultsign = 1'b0;
+        result = {1'b0,1'b1,22'b0};
+        
+        #100; // 您可以根据需要调整时间
+        
+  $finish;
     end
+    
+
 
 endmodule
