13cc3386ac17f80c2f55e8cfb83753079710c411
ChenlinShi <scleverlin@Gmail.com>
2023-10-30 22:25:10 +0900

    update

--- SRT_Divider/csrc/filelist.cu
+++ SRT_Divider/csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _51823_archive_1.so
-_51823_archive_1.so : archive.69/_51823_archive_1.a
+ARCHIVE_OBJS += _69720_archive_1.so
+_69720_archive_1.so : archive.70/_69720_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_51823_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_69720_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_51823_archive_1.so $@
+	@ln -sf .//../simv.daidir//_69720_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.69/_prev_archive_1.a
+_prev_archive_1.so : archive.70/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
--- SRT_Divider/quo_sel_tab.sv
+++ SRT_Divider/quo_sel_tab.sv
@@ -10,7 +10,7 @@ wire         ops_sign;	// opposite sign
 wire   [4:0] r_ori;		// original code of remainder
 wire   [4:0] d_ori;		// original code divisor 
 wire r_ge_0010, r_ge_0011, r_ge_0110, r_ge_0111, r_ge_1000,
-     r_ge_1001, r_ge_1010, r_ge_1011, r_ge_1100; // greater than
+     r_ge_1001, r_ge_1010, r_ge_1011, r_ge_1100,r_ge_1101; // greater than
 logic  q0, q2;		    // quotient value(abs)
 assign ops_sign = r_idx[4] ; 
 assign r_ori = r_idx[4] ? ~r_idx + 1 : r_idx;
@@ -26,21 +26,21 @@ assign r_ge_1001 = (r_ori[3:0]>=4'b1001);
 assign r_ge_1010 = (r_ori[3:0]>=4'b1010);
 assign r_ge_1011 = (r_ori[3:0]>=4'b1011);
 assign r_ge_1100 = (r_ori[3:0]>=4'b1100);
+assign r_ge_1100 = (r_ori[3:0]>=4'b1101);
 
-
-// assign r_ge_0010 = (r_ori[3:0]>4'b0010); // here we use complement code to represent negative number
-// assign r_ge_0011 = (r_ori[3:0]>4'b0011);
-// assign r_ge_0100 = (r_ori[3:0]>4'b0100);
-// assign r_ge_0101 = (r_ori[3:0]>4'b0101);
-// assign r_ge_0110 = (r_ori[3:0]>4'b0110);
-// assign r_ge_0111 = (r_ori[3:0]>4'b0111);
-// assign r_ge_1000 = (r_ori[3:0]>4'b1000);
-// assign r_ge_1001 = (r_ori[3:0]>4'b1001);
-// assign r_ge_1010 = (r_ori[3:0]>4'b1010);
-// assign r_ge_1011 = (r_ori[3:0]>4'b1011);
-// assign r_ge_1100 = (r_ori[3:0]>4'b1100);
-// assign r_ge_0010_r=(r_ori[3:0]>4'b0010);
-// assign r_ge_0110_r=(r_ori[3:0]>4'b0110);
+// assign r_ge_0010_r = (r_ori[3:0]>4'b0010); // here we use complement code to represent negative number
+// assign r_ge_0011_r = (r_ori[3:0]>4'b0011);
+// assign r_ge_0100_r = (r_ori[3:0]>4'b0100);
+// assign r_ge_0101_r = (r_ori[3:0]>4'b0101);
+// assign r_ge_0110_r = (r_ori[3:0]>4'b0110);
+// assign r_ge_0111_r = (r_ori[3:0]>4'b0111);
+// assign r_ge_1000_r = (r_ori[3:0]>4'b1000);
+// assign r_ge_1001_r = (r_ori[3:0]>4'b1001);
+// assign r_ge_1010_r = (r_ori[3:0]>4'b1010);
+// assign r_ge_1011_r = (r_ori[3:0]>4'b1011);
+// assign r_ge_1100_r = (r_ori[3:0]>4'b1100);
+// assign r_ge_0010_r = (r_ori[3:0]>4'b0010);
+// assign r_ge_0110_r =(r_ori[3:0]>4'b0110);
 always_comb begin
 	case(d_ori[3:0])
 		4'b1000: begin
@@ -48,7 +48,7 @@ always_comb begin
 			q2 = ops_sign ? r_ge_0111: r_ge_0110; // complement of remainder is used , not exactly the same as graph
 		end
 		4'b1001: begin
-			q0 = ops_sign ? ~r_ge_0011: ~r_ge_0010;
+			q0 = ops_sign ? ~r_ge_0100: ~r_ge_0011;
 			q2 = ops_sign ? r_ge_1000: r_ge_0111;
 		end
 		4'b1010: begin
@@ -57,11 +57,11 @@ always_comb begin
 		end
 		4'b1011: begin
 			q0 = ops_sign ? ~r_ge_0100 : ~r_ge_0011;
-			q2 = ops_sign ? r_ge_1001 : r_ge_1001;
+			q2 = ops_sign ? r_ge_1010 : r_ge_1001;
 		end
 		4'b1100: begin
 			q0 = ops_sign ? ~r_ge_0101 : ~r_ge_0100;
-			q2 = ops_sign ? r_ge_1010 : r_ge_1010;
+			q2 = ops_sign ? r_ge_1011 : r_ge_1010;
 		end
 		4'b1101: begin
 			q0 = ops_sign ? ~r_ge_0101 : ~r_ge_0100;
@@ -69,11 +69,11 @@ always_comb begin
 		end
 		4'b1110: begin
 			q0 = ops_sign ? ~r_ge_0101 : ~r_ge_0100;
-			q2 = ops_sign ? r_ge_1011 : r_ge_1011;
+			q2 = ops_sign ? r_ge_1100 : r_ge_1011;
 		end
 		4'b1111: begin
-			q0 = ops_sign ? ~r_ge_0101 : ~r_ge_0100;
-			q2 = ops_sign ? r_ge_1100 : r_ge_1100;
+			q0 = ops_sign ? ~r_ge_0101 : ~r_ge_0101;
+			q2 = ops_sign ? r_ge_1101 : r_ge_1100;
 		end
 		default: begin
 			q0 = 1'b1;
