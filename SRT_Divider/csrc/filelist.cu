7edd648408c62f476c177ea93a9bc9f0ef07a091
ChenlinShi <scleverlin@Gmail.com>
2023-10-30 16:16:05 +0900

    update

--- SRT_Divider/csrc/filelist.cu
+++ SRT_Divider/csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _41047_archive_1.so
-_41047_archive_1.so : archive.61/_41047_archive_1.a
+ARCHIVE_OBJS += _41835_archive_1.so
+_41835_archive_1.so : archive.62/_41835_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_41047_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_41835_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_41047_archive_1.so $@
+	@ln -sf .//../simv.daidir//_41835_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.61/_prev_archive_1.a
+_prev_archive_1.so : archive.62/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
--- SRT_Divider/rounding.sv
+++ SRT_Divider/rounding.sv
@@ -1,6 +1,6 @@
 module rounding(current_remainder,current_divisor,rounding_data);
 input logic [25:0] current_remainder,current_divisor;
-output logic [25:0] rounding_data;
+output logic [23:0] rounding_data;
 
 
 logic is_2d_3d;
@@ -12,12 +12,12 @@ logic is_neg3d_neg2d;
 
 remainder_divisor_check check( current_remainder, current_divisor, is_2d_3d, is_d_2d, is_0_d, is_negd_0, is_neg2d_negd, is_neg3d_neg2d);
 
-logic [25:0] one = 26'b1;
-logic [25:0] two = 26'b10;
-logic [25:0] three = 26'b11;
-logic [25:0] minus_three= ~three+one;
-logic [25:0] minus_two= ~two+one;
-logic [25:0] minus_one= ~one+one;
+logic [25:0] one = {24'b1,2'b00};
+logic [25:0] two = {24'b10,2'b00};
+logic [25:0] three = {24'b11,2'b00};
+logic [25:0] minus_three= {~three+one,2'b00};
+logic [25:0] minus_two= {~two+one,2'b00};
+logic [25:0] minus_one= {~one+one,2'b00};
 
 assign rounding_data= (is_2d_3d)?two:(is_d_2d)?one:(is_0_d)?0:(is_negd_0)?minus_one:(is_neg2d_negd)?minus_two:(is_neg3d_neg2d)?minus_three:0;
 
