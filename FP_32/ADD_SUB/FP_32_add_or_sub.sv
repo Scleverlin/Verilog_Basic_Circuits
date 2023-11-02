9b27e7b1b422a561383ca738ccd08ba163af9b6e
shi <shi@shi>
2023-11-02 21:41:26 +0900

    update

--- FP_32/ADD_SUB/FP_32_add_or_sub.sv
+++ FP_32/ADD_SUB/FP_32_add_or_sub.sv
@@ -20,17 +20,36 @@ logic add_or_sub;//1 is add, 0 is sub
 normalization norm (add1,add2,current_add,current_sub,man_a,man_b,exponent_a,exponent_b,result_sign,add_or_sub);
 
 // need consider exponent == 0
-// both exponent == 0, dont need to shift, unless, there is an one in 24th bit
+// both exponent == 0, dont need to shift, but may need to add one in exponent if there is an one in 24th bit
 // if only one exponent == 0, just normal, but need add one in exponent. // E1-E2 or E1-1-0
 
+// for add 
 logic [7:0]e1_e2,e1_e2_0; // how about E1=0 and E2=0?
-logic [7:0]shift;
+logic [7:0]shift_before_add;
 
 assign e1_e2=exponent_a+ ~exponent_b+8'b1;
 assign e1_e2_0=exponent_a+ ~{8'b1}+8'b1;
 
 
+assign shift_before_add= (exponent_a>8'b0&& exponent_b >8'b0)? e1_e2 : (exponent_a>8'b0&&exponent_b==8'b0)? e1_e2_0 :8'b0  ;
 
+logic [7:0]current_exponent_tmp;
+
+assign current_exponent_tmp= (exponent_a>8'b0&& exponent_b >8'b0)? exponent_b : (exponent_a>8'b0&&exponent_b==8'b0)? 8'b1 :8'b0  ;
+
+logic result_shift_or_not;
+
+assign result_shift_or_not= (exponent_a==8'b0&&exponent_b==8'b0)?1'b0:1'b1;// when E1=E2=0, dont need to shift, but may need to add one in exponent if there is an one in 24th bit
+
+logic [7:0]add_one_in_exponent;// 
+
+//for add end
+
+
+//for sub 
+
+
+//for sub end
 
 endmodule
 
