0e5785478bebf8541e1c199439f40c87ccff18f6
ChenlinShi <scleverlin@Gmail.com>
2023-11-22 16:15:21 +0900

    update

--- FP_32/FPU_32/FPU_32.sv
+++ FP_32/FPU_32/FPU_32.sv
@@ -32,34 +32,27 @@ output logic [31:0]result;
 
 logic [31:0]result_mul,result_add,result_div;
 // reg [31:0]a,b;
-
 logic gated_clk_mul,gated_clk_add_or_sub,gated_clk_sub;
 logic mul;
-assign mul=(mode==3'b000)?1'b1:1'b0;
+// assign mul=(mode==3'b000)?1'b1:1'b0;
 logic add;
-assign add=(mode==3'b001)?1'b1:1'b0;
+// assign add=(mode==3'b001)?1'b1:1'b0;
 logic div;
-assign div=(mode==3'b011)?1'b1:1'b0;
+// assign div=(mode==3'b011)?1'b1:1'b0;
 logic sub;
-assign sub=(mode==3'b010)?1'b1:1'b0;
+// assign sub=(mode==3'b010)?1'b1:1'b0;
 
-assign gated_clk_mul = clk & mul;
-assign gated_clk_add_or_sub = clk & (add^sub);
-assign gated_clk_div = clk & div;
+// assign gated_clk_mul = clk & mul;
+// assign gated_clk_add_or_sub = clk & (add^sub);
+// assign gated_clk_div = clk & div;
+func_selector func_selector (mode,gated_clk_mul,gated_clk_add_or_sub,gated_clk_div,clk,mul,add,sub,div);
+
 logic [5:0]flag;
 FP32_mul mul_instance (.clk(gated_clk_mul),.mul1(a),.mul2(b),.product(result_mul));
-
 SRT_divider_FP32 SRT_divider_FP32 (a,b,gated_clk_div,rst,result_div,flag);
-
-logic command;
-assign command =add;
-
-FP_32_add_or_sub add_sub (a,b,command,result_add,gated_clk_add_or_sub);
+FP_32_add_or_sub add_sub (a,b,add,result_add,gated_clk_add_or_sub);
 logic [2:0]mode_reg;
-
-selector select (mode_reg,result_mul,result_add,result_div,result,flag);
-   
-
+result_selector select (mode_reg,result_mul,result_add,result_div,result,flag);
 always @(posedge clk) begin
     if (~rst) begin
      mode_reg<= 3'b111;
@@ -67,11 +60,9 @@ always @(posedge clk) begin
      mode_reg<=mode;
     end
 end
-
-
 endmodule
 
-module selector(mode,result_mul,result_add,result_div,output_r,flag);
+module result_selector(mode,result_mul,result_add,result_div,output_r,flag);
     // 000:mul 001:add 010:sub, 011:div,111:others
 input logic [2:0]mode;
 input logic [5:0] flag;
@@ -88,4 +79,22 @@ always @(*) begin
         default: output_r = 32'b0;
     endcase
 end
+endmodule
+
+module func_selector(mode,gated_clk_mul,gated_clk_add_or_sub,gated_clk_div,clk,mul,add,sub,div);
+input logic [2:0] mode;
+input logic clk;
+output gated_clk_mul,gated_clk_add_or_sub,gated_clk_sub;
+output mul;
+output add;
+output div;
+output sub;
+
+assign mul=(mode==3'b000)?1'b1:1'b0;
+assign add=(mode==3'b001)?1'b1:1'b0;
+assign sub=(mode==3'b010)?1'b1:1'b0;
+assign div=(mode==3'b011)?1'b1:1'b0;
+assign gated_clk_mul = clk & mul;
+assign gated_clk_add_or_sub = clk & (add^sub);
+assign gated_clk_div = clk & div;
 endmodule
