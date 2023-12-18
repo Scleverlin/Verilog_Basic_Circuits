58f0d4e1adf222c840b845891694df4dea488e9e
Chenlin Shi <scleverlin@gmail.com>
2023-12-18 22:51:36 +0900

    pipeline_complete

--- FMA_32_pipelined/tb_fma.sv
+++ FMA_32_pipelined/tb_fma.sv
@@ -1,18 +1,32 @@
-`include "FMA_32_pipelined.sv"
+`include "FMA_32_pipelined_v2.sv"
 
 module FMA_32_tb;
 
     // 输入和输出
     reg [31:0] a, b, c;
     wire [31:0] result;
-
+   logic clk,rst;
     // 实例化FMA_32模块
-    FMA_32_pipelined uut(
+    // FMA_32_pipelined_v2 uut(
+    //     .a(a),
+    //     .b(b),
+    //     .c(c),
+    //     .result(result)
+    // );
+    FMA_clk uut(
+        .clk(clk),
+        .rst(rst),
         .a(a),
         .b(b),
         .c(c),
         .result(result)
     );
+
+ initial begin
+  clk=0;
+     forever #5 clk = ~clk;  
+end 
+
 real a_real, b_real, c_real, result_real;
     // 生成波形文件
     initial begin
@@ -26,7 +40,7 @@ real a_real, b_real, c_real, result_real;
         c = 32'b0;
     //    #10 $display("Test 0: a = %h, b = %h, c = %h, result = %h", a, b, c, result);
         // 简单的测试案例
-        #10 a = 32'h3f800000; // 1.0
+        #5 a = 32'h3f800000; // 1.0
             b = 32'h40000000; // 2.0
             c = 32'h40400000; // 3.0
      #10    a = 32'h48004ABC;   
@@ -39,7 +53,7 @@ real a_real, b_real, c_real, result_real;
         // 这里可以添加更多测试案例来验证不同的输入组合
      #10 $display("Test 0: a = %h, b = %h, c = %h, result = %b", a, b, c, result);
         // 测试结束
-        #10 $finish;
+        #100 $finish;
     end
 
 endmodule
