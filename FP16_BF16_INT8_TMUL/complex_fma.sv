0985f36bb85390e5f510a48aea18ae5d1cb21754
Chenlin Shi <scleverlin@gmail.com>
2024-02-09 13:58:02 +0800

    update

--- FP16_BF16_INT8_TMUL/complex_fma.sv
+++ FP16_BF16_INT8_TMUL/complex_fma.sv
@@ -3,9 +3,19 @@
 module partialproductgenerator (a,zero,one,two,three,four,minus_one,minus_two,minus_three,minus_four);
 input [15:0]a;
 
-// a的11位浮点数需要加一个符号位，不然全是负数
+// a的11位浮点数需要加一个符号位，不然全是负数,所以总共是12位；
 
 output zero; // 0 不需要考虑位数， 只用1位来省导线
+output logic [11:0] one;
+output logic [11:0] minus_one;
+output logic [12:0] two;
+output logic [12:0] minus_two;
+output logic [13:0] three;
+output logic [13:0] minus_three;
+output logic [13:0] four;
+output logic [13:0] minus_four;
+
+
 
 // for two, three, four, need consider the total amount of bits  
 
